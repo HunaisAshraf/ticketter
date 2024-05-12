74f65526d7a6b9590c72ab340350e5d73b2eb379
huniasashraf <hunaisashraf@gmail.com>
2024-05-12 20:39:35 +0530

    error handling middleware

--- auth/src/controller/authController.ts
+++ auth/src/controller/authController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 
 export const getUser = async (req: Request, res: Response) => {
@@ -9,18 +9,22 @@ export const getUser = async (req: Request, res: Response) => {
   }
 };
 
-export const addUser = async (req: Request, res: Response) => {
+export const addUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error("Invalid email or password");
     }
 
     const { email, password } = req.body;
 
-    return res.status(200).send({ success: true, email,password });
+    return res.status(200).send({ success: true, email, password });
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
-import morgan from "morgan"
+import morgan from "morgan";
 import router from "./routes/authRoutes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
-app.use("/api/auth",router);
+app.use("/api/auth", router);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("server running in port 3000!!!");
